a19e155b83d1a7499166a3e1a82a1cfd451e4850
Constantin <constantin@pasqal.io>
2021-03-17 10:01:15 +0100

    fd

--- torchdiffeq/torchdiffeq/_impl/misc.py
+++ torchdiffeq/torchdiffeq/_impl/misc.py
@@ -44,7 +44,7 @@ def _select_initial_step(func, t0, y0, order, rtol, atol, norm, f0=None):
     device = y0.device
     t_dtype = t0.dtype
     t0 = t0.to(dtype)
-    print(f'(in misc._select_initial_step): (y0.device,t0.device is {y0.device,t0.device}')
+    warnings.warn(f'(in misc._select_initial_step): (y0.device,t0.device is {y0.device,t0.device}')
     if f0 is None:
         f0 = func(t0, y0)
 
